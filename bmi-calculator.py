d22ee2e64cafb34acf52b721392675dcd88ca12b
Akafack Jauvany <jauvany@yahoo.com>
2022-07-16 04:30:44 +0300

    Create bmi-calculator.py
    BMI Calculator from scratch

---
+++ bmi-calculator.py
@@ -0,0 +1,18 @@
+#BMI Calculator from scratch.
+w = int(input())
+h = float(input())
+
+fh = h ** 2
+bmi = w / fh
+if bmi < 18.5:
+  print("Underweight")
+else: 
+  if (bmi >= 18.5) and (bmi < 25):
+    print("Normal")
+  else: 
+    if (bmi >= 25) and (bmi < 30): 
+      print("Overweight ")
+    else:
+      if bmi > 30:   
+        print("Obesity")
+
